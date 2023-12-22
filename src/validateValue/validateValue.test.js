7c231ca9027b916994d45ae61e15ed48109a655d
EvGen Gal <genwolk1@gmail.com>
2023-12-23 01:26:32 +0300

    describe(неск.сцен.,PASS), txt.опис.,  pg|pa.json(version,descript)

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -3,7 +3,27 @@ const validateValue = require("./validateValue");
 // ^ преобраз.CommonJS>ES
 // import validateValue from "./validateValue";
 
-// опис.теста(назв., cb проверка)
-test("Валидация значения", () => {
+// опис.теста(назв., cb проверки)
+test("Волидное значение_0", () => {
+  // `ожидать`.fn(знач.).мтд.проверки // ! не показ. > ctrl+пробел
   expect(validateValue(50)).toBe(true);
+});
+
+// опис.неск.сцен.: назв.списка, cb неск.тестов
+describe("validValue", () => {
+  test("Корректное значение", () => {
+    expect(validateValue(55)).toBe(true);
+  });
+  test("Меньше корректного", () => {
+    expect(validateValue(-1)).toBe(false);
+  });
+  test("Больше корректного", () => {
+    expect(validateValue(101)).toBe(false);
+  });
+  test("Пограничное значение снизу", () => {
+    expect(validateValue(0)).toBe(true);
+  });
+  test("Пограничное значение сверху", () => {
+    expect(validateValue(100)).toBe(true);
+  });
 });
