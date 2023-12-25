70360a67524ae49a7144c77be04149f93a3b1f9f
EvGen Gal <genwolk1@gmail.com>
2023-12-25 21:21:54 +0300

    мини правки комм.

--- src/mapArrToString/mapArrToStrings.test.js
+++ src/mapArrToString/mapArrToStrings.test.js
@@ -4,8 +4,9 @@ const mapArrToStrings = require("./mapArrToStrings");
 // опис.неск.сцен.: назв.списка, cb неск.тестов
 describe("mapArrToStrings", () => {
   test("Масс.преобраз.в стр.", () => {
+    // `ожидать`.fn(знач.).мтд.проверки.
+    // ^ toEqual`равный` проверка равенства по значению
     // ! не показ.доп.fn > ctrl+пробел
-    // toEqual`равный` опред.знач.
     expect(mapArrToStrings([1, 2, 3])).toEqual(["1", "2", "3"]);
   });
 });
