b3dc4efcc34dac887f11de84a30dc79dd5c7203c
EvGen Gal <genwolk1@gmail.com>
2023-12-26 22:47:00 +0300

    тесты, коммит.

--- src/mapArrToString/mapArrToStrings.test.js
+++ src/mapArrToString/mapArrToStrings.test.js
@@ -1,12 +1,25 @@
-// imp.fn
+// imp.fn масс.преобраз.в стр.
 const mapArrToStrings = require("./mapArrToStrings");
 
 // опис.неск.сцен.: назв.списка, cb неск.тестов
 describe("mapArrToStrings", () => {
-  test("Масс.преобраз.в стр.", () => {
+  test("Корректное значение", () => {
     // `ожидать`.fn(знач.).мтд.проверки.
-    // ^ toEqual`равный` проверка равенства по значению
+    // ^ toEqual`равный` проверка равенства по значению (глуб.сравн.)
     // ! не показ.доп.fn > ctrl+пробел
     expect(mapArrToStrings([1, 2, 3])).toEqual(["1", "2", "3"]);
+  });
+  test("Мешанина", () => {
+    expect(mapArrToStrings([1, 2, 3, null, undefined, "qwe"])).toEqual([
+      "1",
+      "2",
+      "3",
+    ]);
+  });
+  test("Пустой массив", () => {
+    expect(mapArrToStrings([])).toEqual([]);
+  });
+  test("Отрицание", () => {
+    expect(mapArrToStrings([1, 2, 3])).not.toEqual([1, 2, 3, 4]);
   });
 });
