3c0e518841ae9c0642f4c67989f098e70a24cae3
EvGen Gal <genwolk1@gmail.com>
2024-01-06 17:29:25 +0300

    #app.test.js тест QUERY(нет эл.на стр.), правки коммент

--- src/App.test.js
+++ src/App.test.js
@@ -6,11 +6,12 @@ describe("React Testing Library UTV", () => {
   test("рендеры h1, btn, inp(+снимок)", () => {
     // передача в render Комп.>теста
     render(<App />);
-    // ч/з спец.объ.`экран`.мтд. получ.эл. (по тексту(здесь ч/з регул.выраж.без регистра (//i)), роли, тегу, т.д. и т.п.)
+    // ^ ч/з спец.объ.`экран`.мтд. получ.эл. (по тексту, роли, тегу, т.д. и т.п.)
+    // получ.h1 по тексту ч/з регул.выраж.без регистра (//i)
     const h1RTLibr = screen.getByText(/Привет react testing library/i);
-    // получ.эл.button по роли (у кнп.роль button)
+    // получ.button по роли кнп.
     const btn = screen.getByRole("button");
-    // получ.эл.input по`заполнителю`
+    // получ.input по`заполнителю`
     const input = screen.getByPlaceholderText(/введите значение/i);
     // `ожидание`(эл.)`быть в документе`; (появился в DOM/странице)
     expect(h1RTLibr).toBeInTheDocument();
@@ -22,5 +23,13 @@ describe("React Testing Library UTV", () => {
     // ^ при ошб./не раб./FAIT тест - вызов screen.debug - отрисовка сгенер.разметки в renderе
     // eslint-disable-next-line testing-library/no-debugging-utils
     // screen.debug();
+  });
+
+  test("мтд.screen", () => {
+    render(<App />);
+    // ^ мтд.: GET(поиск.эл.на стр.иначе ошб.), QUERY(нет на стр.), FIND(асинхр.)
+    // отсутств.эл.с текстом
+    const h1RTLibr_2 = screen.queryByText(/Привет react testing library_2/i);
+    expect(h1RTLibr_2).toBeNull();
   });
 });
