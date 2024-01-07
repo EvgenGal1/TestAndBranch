680ed5103824faf719a66d910e4b54c1db6673fd
EvGen Gal <genwolk1@gmail.com>
2024-01-07 21:25:39 +0300

    #app.test.js Комп./тест Find(асинхр); toHaveStyle(стили); getByTestId/fireEvent (id эл.тест, события клик)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  // асинхр.лог. > screen.find + style (ч/з usEf/setTim обнов.usSt > показ.эл.)
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setTimeout(() => { setData({}) }, 500)
+  }, [])
+
+  // лог.событий(fireEvent)/testId/toggle(переключ.)
+  const [toggle, setToggle] = useState(false);
+  const onClick = () => { setToggle(prev => !prev) }
+
   return (
     <div className="App">
+      {/* асинхр.лог. > screen.find + style */}
+      {data && <div style={{ color: "red" }}>data</div>}
+      {/* лог.событий(fireEvent)/testId/toggle(переключ.) */}
+      {toggle === true && <div data-testid="toggle-elem">toggle</div>}
       <h1>Привет React Testing Library</h1>
-      <button>нажми на меня</button>
+      {/* btn + testId(получ.эл.) + слуш.событ. > toggle(переключ.) */}
+      <button data-testid="toggle-btn" onClick={onClick}>нажми на меня</button>
       <input type="text" placeholder="введите значение..." />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import App from "./App";
 
 describe("React Testing Library UTV", () => {
+  // получ.эл./снимок
   test("рендеры h1, btn, inp(+снимок)", () => {
     // передача в render Комп.>теста
     render(<App />);
-    // ^ ч/з спец.объ.`экран`.мтд. получ.эл. (по тексту, роли, тегу, т.д. и т.п.)
+    // ^ ч/з спец.объ.`экран`.мтд. получ.эл. (по тексту, роли, тегу, стилю, props, т.д. и т.п.)
     // получ.h1 по тексту ч/з регул.выраж.без регистра (//i)
     const h1RTLibr = screen.getByText(/Привет react testing library/i);
     // получ.button по роли кнп.
@@ -25,11 +26,49 @@ describe("React Testing Library UTV", () => {
     // screen.debug();
   });
 
-  test("мтд.screen", () => {
+  // GET мтд.screen
+  test("screen.QUERY", () => {
     render(<App />);
     // ^ мтд.: GET(поиск.эл.на стр.иначе ошб.), QUERY(нет на стр.), FIND(асинхр.)
-    // отсутств.эл.с текстом
+    // ожидается отсутств.эл.с текстом
     const h1RTLibr_2 = screen.queryByText(/Привет react testing library_2/i);
     expect(h1RTLibr_2).toBeNull();
+  });
+
+  // FIND async мтд.screen
+  test("screen.FIND", async () => {
+    render(<App />);
+    // screen.debug(); // ^ render БЕЗ div > data
+    // `ожидание`.эл.с текстом
+    const h1RTLibr_3 = await screen.findByText(/data/i);
+    expect(h1RTLibr_3).toBeInTheDocument();
+    // screen.debug(); // ^ render С div > data
+  });
+
+  // получ.эл.по style
+  test("screen.STYLE", async () => {
+    render(<App />);
+    const h1RTLibr_3 = await screen.findByText(/data/i);
+    expect(h1RTLibr_3).toHaveStyle({ color: "red" });
+  });
+
+  // события/клик/testId
+  test("EVENT click", async () => {
+    render(<App />);
+    // получ.эл.по testId
+    const btn = screen.getByTestId("toggle-btn");
+    // получ.эл. > query т.к. эл.изначально нет // ! ошб. т.к. эл.записан в перем.до появления // ~ перенос лог.в expect
+    // const toggleDiv = screen.queryByTestId("toggle-elem");
+    // ожидается отсутств.эл.
+    expect(screen.queryByTestId("toggle-elem")).toBeNull();
+    // ^ ч/з спец.объ.`огненное событие`.мтд. получ.событие эл. (по клику, измен., копир., мышь, т.д. и т.п.)
+    // клику на toggle-btn
+    fireEvent.click(btn);
+    // ожидается появление.эл. // !! использ.get от предупрежд.eslint
+    expect(screen.getByTestId("toggle-elem")).toBeInTheDocument();
+    // повторный клик на toggle-btn > проверка откл.toggle-elem
+    fireEvent.click(btn);
+    // повторно ожидается отсутств.эл.
+    expect(screen.queryByTestId("toggle-elem")).toBeNull();
   });
 });
